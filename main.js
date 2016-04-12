43cca5caf8e5902dc5e6ad05c31f08df8843e321
Felipe Rodrigues <felipe.rodrigues@rocket-internet.com.br>
2016-04-12 16:25:48 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,36 @@
+(function($) {
+
+  var randomExcuses = [
+      "É problema de cache, limpa o cache do seu navegador...",
+      "É problema da API!",
+      "Isso só acontece no retina..."
+  ];
+
+  function getRandomExcuse() {
+    return randomExcuses[Math.floor(Math.random() * randomExcuses.length)];
+  }
+
+  $.fn.fadeTo = function(target) {
+    var e = $(this);
+    e.fadeOut(300, "swing", function() {
+      e.html(target);
+      e.fadeIn(200, "swing");
+    })
+  };
+
+  $(document).keyup(function(k) {
+    if (k.keyCode == 32) {
+      $("h1").fadeTo(getRandomExcuse());
+    }
+  });
+
+  $(".tweet").click(function(a) {
+    a.preventDefault();
+
+    var b = encodeURIComponent('"' + $("h1").html() + '" - Gere a sua desculpa completamente aleatória no');
+    window.open("http://twitter.com/share?url=&text=" + b + "&related=ferodss&", "twitterwindow", "height=450, width=550, top=" + ($(window).height() / 2 - 225) + ", left=" + $(window).width() / 2 + ", toolbar=0, location=0, menubar=0, directories=0, scrollbars=0")
+  });
+
+  $("h1").fadeTo(getRandomExcuse());
+
+})(jQuery);
